6f1bddda83d343194b90d53070a9ce4639fe5fb4
John Forsythe <john@blamcast.net>
2020-03-28 17:05:03 -0400

    Implement send-to-fingers mode

--- src/Pushy.cpp
+++ src/Pushy.cpp
@@ -1,8 +1,22 @@
+// ideas:
+// only send changes to touched knobs (1-4)
+// 5-8 become control knobs
+// keep last touched knobs active
+// can control multiple knobs
+// only clear active knobs when starting a new touch
+/*
+ *  v v v v    base voltage
+ *  a a a a    amplitude of osc
+ *  f f f f    freq of osc
+ *  s s s s    shape of osc
+ *  o o o o    outputs
+ */
+// make sine default shape
+// set other knobs defaults
+
 #include "plugin.hpp"
 
-static const int KNOBS = 8;
-static const float STEPSIZE = 0.1f;
-//static const double U = 5.08f;
+static const int KNOBS = 4;
 
 struct Pushy : Module {
     enum ParamIds {
@@ -14,6 +28,14 @@ struct Pushy : Module {
         KNOB6_PARAM,
         KNOB7_PARAM,
         KNOB8_PARAM,
+        KNOB9_PARAM,
+        KNOB10_PARAM,
+        KNOB11_PARAM,
+        KNOB12_PARAM,
+        KNOB13_PARAM,
+        KNOB14_PARAM,
+        KNOB15_PARAM,
+        KNOB16_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
@@ -24,21 +46,24 @@ struct Pushy : Module {
         OUT2_OUTPUT,
         OUT3_OUTPUT,
         OUT4_OUTPUT,
-        OUT5_OUTPUT,
-        OUT6_OUTPUT,
-        OUT7_OUTPUT,
-        OUT8_OUTPUT,
         NUM_OUTPUTS
     };
     enum LightIds {
-        FREQ_LIGHT,
+        LIGHT1_LIGHT,
+        LIGHT2_LIGHT,
+        LIGHT3_LIGHT,
+        LIGHT4_LIGHT,
         NUM_LIGHTS
     };
     enum Modes {
+        BASE,
         AMPLITUDE,
         FREQUENCY,
         SHAPE,
+        NUM_MODES
     };
+
+    dsp::ExponentialSlewLimiter slewLimiter[KNOBS][NUM_MODES];
 
     Pushy() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
@@ -50,32 +75,48 @@ struct Pushy : Module {
         configParam(KNOB6_PARAM, 0.f, 10.f, 0.f, "");
         configParam(KNOB7_PARAM, 0.f, 10.f, 0.f, "");
         configParam(KNOB8_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB9_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB10_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB11_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB12_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB13_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB14_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB15_PARAM, 0.f, 10.f, 0.f, "");
+        configParam(KNOB16_PARAM, 0.f, 10.f, 0.f, "");
         onReset();
     }
 
-    int8_t values[128];
     int ccs[128];
     float phase[KNOBS];
-    float frequency[KNOBS];
-    float amplitude[KNOBS];
-    float shape[KNOBS];
-    u_int8_t mode = AMPLITUDE;
-    u_int8_t prevMode = FREQUENCY;
+    //float frequency[KNOBS];
+    //float amplitude[KNOBS];
+    //float shape[KNOBS];
+    float values[KNOBS][NUM_MODES];
+    bool active[4];
+
+    float stepSize[NUM_MODES];
 
     midi::InputQueue midiInput;
 
     void onReset() override {
+        stepSize[BASE] = 0.1;
+        stepSize[AMPLITUDE] = 0.1;
+        stepSize[FREQUENCY] = 0.05;
+        stepSize[SHAPE] = 0.05;
+
         for (int i = 0; i < KNOBS; i++) {
+            active[i] = false;
             phase[i] = 0;
-            frequency[i] = 0;
-            amplitude[i] = 0;
-            shape[i] = 0;
+            for (int mode = 0; mode < NUM_MODES; mode++) {
+                values[i][mode] = 0;
+                slewLimiter[i][mode].setRiseFall(1000, 1000);
+            }
         }
+
         midiInput.reset();
     }
 
     void process(const ProcessArgs &args) override {
-        prevMode = mode;
 //        lights[FREQ_LIGHT].setBrightness(1.0f);
 
         midi::Message msg;
@@ -85,19 +126,26 @@ struct Pushy : Module {
         }
 
         for (int i = 0; i < KNOBS; i++) {
-            float freq = powf(1.36f, frequency[i]) - 1;
+            // do knobs -> values slew
+            for (int mode = 0; mode < NUM_MODES; mode++) {
+                int n = i + (4 * mode);
+                values[i][mode] = slewLimiter[i][mode].process(args.sampleTime, params[n].getValue());
+            }
 
-            // Accumulate the phase
+            // do oscillations
+            float freq = powf(1.36f, values[i][FREQUENCY]) - 1;
+
+            // accumulate the phase
             phase[i] += freq * args.sampleTime;
             if (phase[i] >= 2.f)
                 phase[i] -= 2.f;
 
-            // Compute the sine output
+            // invert phase
             float x = 2 - phase[i];
 
             // todo: store when knob is turned
-            int algo = int(shape[i] / 2);
-            float mix = fmod(shape[i] / 2, 1);
+            int algo = int(values[i][SHAPE] / 2);
+            float mix = fmod(values[i][SHAPE] / 2, 1);
             float invertMix = 1;
             float algo1 = 0;
             float algo2 = 0;
@@ -112,10 +160,6 @@ struct Pushy : Module {
                             (x / -mix) + 1,
                             ((-x + 4 * mix - 2) / (mix - 1)) - 3
                     );
-                    //algo1 = fmax(
-                    //        (-x / (1 - mix)) + 1,
-                    //        (( x + 4 * mix - 2) / mix) - 3
-                    //);
                     break;
                 case 1:
                     // linear sweep to sine
@@ -156,34 +200,30 @@ struct Pushy : Module {
             }
             float y = invertMix * algo1 + mix * algo2;
 
-            //switch (int(shape[i])) {
-            //    printf("%d",int);
-            //}
-            //float y = abs((2 * x - 1) - 1) - 1;
-            //if (i == 0) printf("phase %f, y %f\n", x, y);
-
             // todo: store when knob is turned, don't recalculate
-            float amp = powf(1.27098f, amplitude[i]) - 1;
+            float amp = powf(1.27098f, values[i][AMPLITUDE]) - 1;
             // output 0-10v, centered on +5v
-            outputs[i].setVoltage(5.0f + y * amp / 2.0f);
+            float osc = 5.0f + y * amp / 2.0f;
+
+            outputs[i].setVoltage(values[i][BASE]+osc-5.0f);
         }
 
         // reset knob values on mode change
-        if (mode != prevMode) {
-            for (int i = 0; i < KNOBS; i++) {
-                switch (mode) {
-                    case AMPLITUDE:
-                        params[i].setValue(amplitude[i]);
-                        break;
-                    case FREQUENCY:
-                        params[i].setValue(frequency[i]);
-                        break;
-                    case SHAPE:
-                        params[i].setValue(shape[i]);
-                        break;
-                }
-            }
-        }
+        //if (mode != prevMode) {
+        //    for (int i = 0; i < KNOBS; i++) {
+        //        switch (mode) {
+        //            case AMPLITUDE:
+        //                params[i].setValue(amplitude[i]);
+        //                break;
+        //            case FREQUENCY:
+        //                params[i].setValue(frequency[i]);
+        //                break;
+        //            case SHAPE:
+        //                params[i].setValue(shape[i]);
+        //                break;
+        //        }
+        //    }
+        //}
     }
 
     void processMessage(midi::Message msg) {
@@ -193,21 +233,14 @@ struct Pushy : Module {
                 break;
             }
             case 0x9: { // note on
-                if (msg.getNote() == 10) {
+                uint8_t i = msg.getNote();
+                if (i < 4) {
                     if (msg.getValue() == 0) {
-                        mode = AMPLITUDE;
-                        lights[FREQ_LIGHT].setBrightness(0.0f);
+                        active[i] = false;
+                        lights[i].setBrightness(0.0f);
                     } else {
-                        mode = FREQUENCY;
-                        lights[FREQ_LIGHT].setBrightness(1.0f);
-                    }
-                } else if (msg.getNote() == 9) {
-                    if (msg.getValue() == 0) {
-                        mode = AMPLITUDE;
-                        lights[FREQ_LIGHT].setBrightness(0.0f);
-                    } else {
-                        mode = SHAPE;
-                        lights[FREQ_LIGHT].setBrightness(0.5f);
+                        active[i] = true;
+                        lights[i].setBrightness(1.0f);
                     }
                 }
 
@@ -219,47 +252,40 @@ struct Pushy : Module {
     void processCC(midi::Message msg) {
         uint8_t cc = msg.getNote();
 
-        // ignore things that aren't the knobs
+        // ignore things that aren't the knobs 1-8
         if (cc < 71 || cc > 78) return;
 
-        int knob = cc - 71;
+        int knob = cc - 71 + 1;
+        //printf("knob %d\n", knob);
 
         int8_t value = msg.bytes[2];
         value = clamp(value, 0, 127);
 
-        params[71 - cc].getValue();
-
-//        float v = outputs[knob].getVoltage();
-        float v;
-        switch (mode) {
-            case AMPLITUDE: {
-                v = amplitude[knob];
-                v += value <= uint8_t(63) ? value * STEPSIZE : (128 - value) * -STEPSIZE;
-                v = clamp(v, 0.f, 10.f);
-                amplitude[knob] = v;
-                break;
-            }
-            case FREQUENCY: {
-                v = frequency[knob];
-                v += value <= uint8_t(63) ? value * STEPSIZE * 0.5f : (128 - value) * -STEPSIZE * 0.5f;
-                v = clamp(v, 0.f, 10.f);
-                frequency[knob] = v;
-                break;
-            }
-            case SHAPE: {
-                v = shape[knob];
-                v += value <= uint8_t(63) ? value * STEPSIZE * 0.5f : (128 - value) * -STEPSIZE * 0.5f;
-                v = clamp(v, 0.f, 10.f);
-                shape[knob] = v;
-                break;
+        if (knob <= 4) {
+            float v = params[knob-1].getValue();
+            // account for knob acceleration
+            v += value <= uint8_t(63) ? value * stepSize[BASE] : (128 - value) * -stepSize[BASE];
+            v = clamp(v, 0.f, 10.f);
+            params[knob-1].setValue(v);
+        }
+        if (knob > 4) {
+            float v;
+            // knobs: 5 = base, 6 = amplitude, 7 = frequency, 8 = shape
+            // modes: 0 = base, 1 = amplitude, 2 = frequency, 3 = shape
+            int mode = knob - 5;
+            printf("knob %d, mode %d\n", knob, mode);
+            for (int i = 0; i<KNOBS; i++) {
+                if (active[i]) {
+                    int n = i + (4 * mode);
+                    printf("setting param %d\n", n);
+                    v = params[n].getValue();
+                    // account for knob acceleration
+                    v += value <= uint8_t(63) ? value * stepSize[mode] : (128 - value) * -stepSize[mode];
+                    v = clamp(v, 0.f, 10.f);
+                    params[n].setValue(v);
+                }
             }
         }
-
-        // account for knob acceleration values
-//        v += value <= uint8_t(63) ? value * STEPSIZE : (128 - value) * -STEPSIZE;
-//        v = clamp(v, 0.f, 10.f);
-
-        params[knob].setValue(v);
 
     }
 
@@ -311,26 +337,32 @@ struct PushyWidget : ModuleWidget {
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 29.44)), module, Pushy::KNOB1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 29.44)), module, Pushy::KNOB2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 29.44)), module, Pushy::KNOB3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 29.44)), module, Pushy::KNOB4_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 42.14)), module, Pushy::KNOB5_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 42.14)), module, Pushy::KNOB6_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 42.14)), module, Pushy::KNOB7_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 42.14)), module, Pushy::KNOB8_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 25.63)), module, Pushy::KNOB1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 25.63)), module, Pushy::KNOB2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 25.63)), module, Pushy::KNOB3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 25.63)), module, Pushy::KNOB4_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 38.33)), module, Pushy::KNOB5_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 38.33)), module, Pushy::KNOB6_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 38.33)), module, Pushy::KNOB7_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 38.33)), module, Pushy::KNOB8_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 51.03)), module, Pushy::KNOB9_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 51.03)), module, Pushy::KNOB10_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 51.03)), module, Pushy::KNOB11_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 51.03)), module, Pushy::KNOB12_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 63.73)), module, Pushy::KNOB13_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 63.73)), module, Pushy::KNOB14_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 63.73)), module, Pushy::KNOB15_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 63.73)), module, Pushy::KNOB16_PARAM));
 
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 54.84)), module, Pushy::OUT1_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 54.84)), module, Pushy::OUT2_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 54.84)), module, Pushy::OUT3_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 54.84)), module, Pushy::OUT4_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 67.54)), module, Pushy::OUT5_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 67.54)), module, Pushy::OUT6_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 67.54)), module, Pushy::OUT7_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 67.54)), module, Pushy::OUT8_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 76.43)), module, Pushy::OUT1_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 76.43)), module, Pushy::OUT2_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 76.43)), module, Pushy::OUT3_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 76.43)), module, Pushy::OUT4_OUTPUT));
 
-        addChild(createLightCentered<SmallLight<GreenLight>>(mm2px(Vec(13.97 + 1.27, 54.84)), module,
-                                                             Pushy::FREQ_LIGHT));
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(8.89, 18.01)), module, Pushy::LIGHT1_LIGHT));
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(21.59, 18.01)), module, Pushy::LIGHT2_LIGHT));
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(34.29, 18.01)), module, Pushy::LIGHT3_LIGHT));
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(46.99, 18.01)), module, Pushy::LIGHT4_LIGHT));
 
         PushyMidiWidget *midiWidget = createWidget<PushyMidiWidget>(mm2px(Vec(3.41891, 128.5 - 28 - 2.54)));
         midiWidget->box.size = mm2px(Vec(49.08, 22.92));
--- src/Pushy2.cpp
+++ src/Pushy2.cpp
@@ -11,6 +11,14 @@
 //		KNOB6_PARAM,
 //		KNOB7_PARAM,
 //		KNOB8_PARAM,
+//		KNOB9_PARAM,
+//		KNOB10_PARAM,
+//		KNOB11_PARAM,
+//		KNOB12_PARAM,
+//		KNOB13_PARAM,
+//		KNOB14_PARAM,
+//		KNOB15_PARAM,
+//		KNOB16_PARAM,
 //		NUM_PARAMS
 //	};
 //	enum InputIds {
@@ -21,13 +29,13 @@
 //		OUT2_OUTPUT,
 //		OUT3_OUTPUT,
 //		OUT4_OUTPUT,
-//		OUT5_OUTPUT,
-//		OUT6_OUTPUT,
-//		OUT7_OUTPUT,
-//		OUT8_OUTPUT,
 //		NUM_OUTPUTS
 //	};
 //	enum LightIds {
+//		LIGHT1_LIGHT,
+//		LIGHT2_LIGHT,
+//		LIGHT3_LIGHT,
+//		LIGHT4_LIGHT,
 //		NUM_LIGHTS
 //	};
 //
@@ -41,6 +49,14 @@
 //		configParam(KNOB6_PARAM, 0.f, 1.f, 0.f, "");
 //		configParam(KNOB7_PARAM, 0.f, 1.f, 0.f, "");
 //		configParam(KNOB8_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB9_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB10_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB11_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB12_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB13_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB14_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB15_PARAM, 0.f, 1.f, 0.f, "");
+//		configParam(KNOB16_PARAM, 0.f, 1.f, 0.f, "");
 //	}
 //
 //	void process(const ProcessArgs& args) override {
@@ -58,23 +74,32 @@
 //		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 //		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 //
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 29.44)), module, Pushy::KNOB1_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 29.44)), module, Pushy::KNOB2_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 29.44)), module, Pushy::KNOB3_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 29.44)), module, Pushy::KNOB4_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 42.14)), module, Pushy::KNOB5_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 42.14)), module, Pushy::KNOB6_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 42.14)), module, Pushy::KNOB7_PARAM));
-//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 42.14)), module, Pushy::KNOB8_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 25.63)), module, Pushy::KNOB1_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 25.63)), module, Pushy::KNOB2_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 25.63)), module, Pushy::KNOB3_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 25.63)), module, Pushy::KNOB4_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 38.33)), module, Pushy::KNOB5_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 38.33)), module, Pushy::KNOB6_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 38.33)), module, Pushy::KNOB7_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 38.33)), module, Pushy::KNOB8_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 51.03)), module, Pushy::KNOB9_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 51.03)), module, Pushy::KNOB10_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 51.03)), module, Pushy::KNOB11_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 51.03)), module, Pushy::KNOB12_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.89, 63.73)), module, Pushy::KNOB13_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.59, 63.73)), module, Pushy::KNOB14_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.29, 63.73)), module, Pushy::KNOB15_PARAM));
+//		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.99, 63.73)), module, Pushy::KNOB16_PARAM));
 //
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 54.84)), module, Pushy::OUT1_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 54.84)), module, Pushy::OUT2_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 54.84)), module, Pushy::OUT3_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 54.84)), module, Pushy::OUT4_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 67.54)), module, Pushy::OUT5_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 67.54)), module, Pushy::OUT6_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 67.54)), module, Pushy::OUT7_OUTPUT));
-//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 67.54)), module, Pushy::OUT8_OUTPUT));
+//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 76.43)), module, Pushy::OUT1_OUTPUT));
+//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.59, 76.43)), module, Pushy::OUT2_OUTPUT));
+//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.29, 76.43)), module, Pushy::OUT3_OUTPUT));
+//		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(46.99, 76.43)), module, Pushy::OUT4_OUTPUT));
+//
+//		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(8.89, 18.01)), module, Pushy::LIGHT1_LIGHT));
+//		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(21.59, 18.01)), module, Pushy::LIGHT2_LIGHT));
+//		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(34.29, 18.01)), module, Pushy::LIGHT3_LIGHT));
+//		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(46.99, 18.01)), module, Pushy::LIGHT4_LIGHT));
 //	}
 //};
 //
